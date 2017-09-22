e98985ae6884761de333d6bada2cd120e50c0df6
nadav <tashernadav@gmail.com>
2017-09-22 13:13:28 -0400

    Version 0.0.0.0.0.4

--- app/src/main/java/school/nadav/tasher/trys/Main.java
+++ app/src/main/java/school/nadav/tasher/trys/Main.java
@@ -2,11 +2,13 @@ package school.nadav.tasher.trys;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Main extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toast.makeText(this,"Hello",Toast.LENGTH_LONG).show();
     }
 }
