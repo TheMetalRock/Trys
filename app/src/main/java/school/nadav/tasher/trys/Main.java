3cd9eef9f972a4194fe396dd78b7469d388986ae
nadav <tashernadav@gmail.com>
2017-09-22 12:03:37 -0400

    Version 0.0.0.0.0.3

--- app/src/main/java/school/nadav/tasher/trys/Main.java
+++ app/src/main/java/school/nadav/tasher/trys/Main.java
@@ -2,11 +2,14 @@ package school.nadav.tasher.trys;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class Main extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        LinearLayout ll=new LinearLayout(this);
+        setContentView(ll);
     }
 }
