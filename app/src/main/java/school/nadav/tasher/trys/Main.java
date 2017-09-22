dfc43637cd08ea9b583e64d74f844b04f836dd6f
nadav <tashernadav@gmail.com>
2017-09-22 11:59:23 -0400

    Initial commit

---
+++ app/src/main/java/school/nadav/tasher/trys/Main.java
@@ -0,0 +1,12 @@
+package school.nadav.tasher.trys;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
